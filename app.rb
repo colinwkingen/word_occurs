dd5985ec14063f87b31778baf617821fb59a0099
Colin Kingen <colinwkingen@gmail.com>
2016-07-01 09:54:17 -0700

    connect new string to resultpage

--- app.rb
+++ app.rb
@@ -7,9 +7,9 @@ get('/') do
   erb(:index)
 end
 get('/result') do
-  @formatted_sentence = @sentence.string_process()
   @sentence = params.fetch('sentence')
   @target_word = params.fetch('target_word')
+  @formatted_sentence = @sentence.string_process()
   @result = @formatted_sentence.word_occurs(@target_word)
   erb(:result)
 end
